35e619ddb9173093656fe0f5a2fbadaecb882d79
DanielZustik <danielzustik@gmail.com>
2024-02-20 09:19:51 +0100

    ...

---
+++ src/fromUdemy/INTERFACES/Main.java
@@ -0,0 +1,23 @@
+package fromUdemy.INTERFACES;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Mappable> mappables = new ArrayList<>();
+
+        mappables.add(new Building("Sydney Town Hall", UsageType.GOVERNMENT));
+        mappables.add(new Building("Sydney Opera House", UsageType.ENTERTAINMENT));
+        mappables.add(new Building("Stadium Australia", UsageType.SPORTS));
+
+        mappables.add(new UtilityLine("College St", UtilityType.FIBER_OPTIC));
+        mappables.add(new UtilityLine("Olumpic Blvd", UtilityType.WATER));
+
+        for (Mappable m : mappables) {
+            Mappable.mapIt(m);
+        }
+    }
+}
